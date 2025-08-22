ab93a44dc5cc82b79ab0423b37e04629feb9dd8e
cfk <97732318+ceyaima@users.noreply.github.com>
2025-08-22 23:15:32 +0700

    Update and rename userconfig.example.js to userconfig.js

---
+++ userconfig.js
@@ -0,0 +1,202 @@
+// User configuration for the startpage - update the palette, location, and your preferred tabs, categories, and links
+
+// Define preferred palette for light and dark mode
+// Available themes: latte, frappe, mocha, macchiato
+const preferredLightTheme = latte;
+const preferredDarkTheme = mocha;
+
+let palette = initThemeSystem(preferredLightTheme, preferredDarkTheme);
+
+const default_configuration = {
+  overrideStorage: true,
+  temperature: {
+    location: "Jakarta",
+    scale: "C",
+  },
+  clock: {
+    format: "h:i P",
+    icon_color: palette.maroon,
+  },
+  additionalClocks: [
+    {
+      label: "HK",
+      timezone: "HongKong/HongKong",
+      format: "h:i",
+      icon_color: palette.peach,
+    },
+  ],
+  search: {
+    engines: {
+      p: ["https://www.perplexity.ai/search/?q=", "PerplexityAI"],
+      d: ["https://duckduckgo.com/?q=", "DuckDuckGo"],
+      g: ["https://google.com/search?q=", "Google"],
+    },
+    default: "g",
+  },
+  keybindings: {
+    "s": "search-bar",
+  },
+  disabled: [],
+  localIcons: true,
+  localFonts: true,
+  fastlink: "https://www.perplexity.ai",
+  openLastVisitedTab: true,
+  tabs: [
+    {
+      name: "anilist",
+      background_url: "src/img/banners/banner_09.gif",
+      categories: [
+        {
+          name: "アニメ",
+          links: [
+            {
+              name: "watch",
+              url: "https://animepahe.ru",
+              icon: "player-play",
+              icon_color: palette.maroon,
+            },
+            {
+              name: "track",
+              url: "https://anilist.me/croixph",
+              icon: "pencil-plus",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "描く",
+          links: [
+            {
+              name: "thread",
+              url: "https://anilist.co/forum/thread/81749",
+              icon: "needle-thread",
+              icon_color: palette.peach,
+            },
+            {
+              name: "database",
+              url: "https://docs.google.com/spreadsheets/d/1JndPm9jyhTHiMvDwBypA02HwRz9Epe5gheHwK43RapQ/edit?usp=sharing",
+              icon: "database",
+              icon_color: palette.green,
+            },
+            {
+              name: "album",
+              url: "https://imgur.com/a/croixphs-badges-ctrl-f-character-name-NsDvcaq",
+              icon: "library-photo",
+              icon_color: palette.teal,
+            },
+            {
+              name: "archive",
+              url: "https://drive.google.com/drive/u/3/folders/1tQNxuEdGAFS-1_GIhLmM0cO7Z_MUTiPP",
+              icon: "brand-google-drive",
+              icon_color: palette.red,
+            },
+          ],
+        },
+        {
+          name: "挑戦",
+          links: [
+            {
+              name: "enlist",
+              url: "https://anilist.co/activity/26266744",
+              icon: "login-2",
+              icon_color: palette.sapphire,
+            },
+            {
+              name: "casefiles",
+              url: "https://imgur.com/a/awc-stuff-bqoADrf",
+              icon: "folder-open",
+              icon_color: palette.sapphire,
+            },
+            {
+              name: "calculator",
+              url: "https://awc.moe/calculators",
+              icon: "calculator",
+              icon_color: palette.sapphire,
+            },
+            {
+              name: "submit",
+              url: "https://anilist.co/forum/thread/38857",
+              icon: "flag-3",
+              icon_color: palette.sapphire,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "work",
+      background_url: "src/img/banners/banner_07.gif",
+      categories: [
+        {
+          name: "study",
+          links: [
+            {
+              name: "e-student",
+              url: "https://www38.polyu.edu.hk/eStudent/",
+              icon: "school",
+              icon_color: palette.maroon,
+            },
+            {
+              name: "gen-ai",
+              url: "https://genai.polyu.edu.hk/",
+              icon: "brain",
+              icon_color: palette.teal,
+            },
+            {
+              name: "mail",
+              url: "https://outlook.office.com/mail/?realm=connect.polyu.hk&login_hint=25089289d@connect.polyu.hk",
+              icon: "mail",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "scans",
+          links: [
+            {
+              name: "drive",
+              url: "https://drive.google.com/drive/u/2/starred",
+              icon: "brand-google-drive",
+              icon_color: palette.green,
+            },
+            {
+              name: "asylum",
+              url: "https://docs.google.com/spreadsheets/d/1rWQqiLt4pN_NzSBI5xZ4bWB7qiZDVJ6w/edit?gid=1511134320#gid=1511134320",
+              icon: "file-spreadsheet",
+              icon_color: palette.rosewater,
+            },
+            {
+              name: "gg",
+              url: "https://docs.google.com/spreadsheets/u/2/d/1pBTrOZse-MaisXVEJTmPbKo7huZoL90syVSXJlFt2QU/edit?usp=drive_web&ouid=108792894511475168143",
+              icon: "file-spreadsheet",
+              icon_color: palette.flamingo,
+            },
+          ],
+        },
+        {
+          name: "resources",
+          links: [
+            {
+              name: "avast-ye",
+              url: "https://rentry.co/megathread",
+              icon: "blockquote",
+              icon_color: palette.sky,
+            },
+            {
+              name: "otsu",
+              url: "https://wotaku.wiki/",
+              icon: "planet",
+              icon_color: palette.sapphire,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const CONFIG = new Config(default_configuration, palette);
+
+const root = document.querySelector(":root");
+root.style.setProperty("--bg", palette.mantle);
+root.style.setProperty("--accent", palette.green);
